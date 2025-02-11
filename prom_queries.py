90241cb854b1ec69931494ce9e44777da5cbdb4f
dlamagna <davidelamagna17@gmail.com>
2025-02-11 09:31:33 +0000

    updated multi service trace

--- prom_queries.py
+++ prom_queries.py
@@ -30,16 +30,16 @@ PROMETHEUS_QUERIES = {
     "network_transmit": 'sum(rate(container_network_transmit_bytes_total[5m])) by (pod)',
     
     # CPU Consumption by Service
-    "cpu_consumption_compose": 'sum by (service) (rate(container_cpu_usage_seconds_total{namespace="default", container=~"compose.*"}[2m]))',
-    "cpu_consumption_nginx": 'sum by (service) (rate(container_cpu_usage_seconds_total{namespace="default", container=~"nginx.*"}[2m]))',
-    "cpu_consumption_text": 'sum by (service) (rate(container_cpu_usage_seconds_total{namespace="default", container=~"text.*"}[2m]))',
-    "cpu_consumption_user_mention": 'sum by (service) (rate(container_cpu_usage_seconds_total{namespace="default", container=~"user-mention.*"}[2m]))',
+    "cpu_consumption_compose": 'sum by (service) (rate(container_cpu_usage_seconds_total{namespace="socialnetwork", container=~"compose.*"}[2m]))',
+    "cpu_consumption_nginx": 'sum by (service) (rate(container_cpu_usage_seconds_total{namespace="socialnetwork", container=~"nginx.*"}[2m]))',
+    "cpu_consumption_text": 'sum by (service) (rate(container_cpu_usage_seconds_total{namespace="socialnetwork", container=~"text.*"}[2m]))',
+    "cpu_consumption_user_mention": 'sum by (service) (rate(container_cpu_usage_seconds_total{namespace="socialnetwork", container=~"user-mention.*"}[2m]))',
     
     # CPU Utilization per Service
-    "cpu_utilization_compose": 'sum(rate(container_cpu_usage_seconds_total{namespace="default", container=~"compose.*"}[2m])) / sum(kube_pod_container_resource_limits{resource="cpu", namespace="default", container=~"compose.*", service="kube-state-metrics"}) * 100',
-    "cpu_utilization_nginx": 'sum(rate(container_cpu_usage_seconds_total{namespace="default", container=~"nginx.*"}[2m])) / sum(kube_pod_container_resource_limits{resource="cpu", namespace="default", container=~"nginx.*", service="kube-state-metrics"}) * 100',
-    "cpu_utilization_text": 'sum(rate(container_cpu_usage_seconds_total{namespace="default", container=~"text-service.*"}[2m])) / sum(kube_pod_container_resource_limits{resource="cpu", namespace="default", container=~"text-service.*", service="kube-state-metrics"}) * 100',
-    "cpu_utilization_user_mention": 'sum(rate(container_cpu_usage_seconds_total{namespace="default", container=~"user-mention.*"}[2m])) / sum(kube_pod_container_resource_limits{resource="cpu", namespace="default", container=~"user-mention.*", service="kube-state-metrics"}) * 100',
+    "cpu_utilization_compose": 'sum(rate(container_cpu_usage_seconds_total{namespace="socialnetwork", container=~"compose.*"}[2m])) / sum(kube_pod_container_resource_limits{resource="cpu", namespace="socialnetwork", container=~"compose.*", service="kube-state-metrics"}) * 100',
+    "cpu_utilization_nginx": 'sum(rate(container_cpu_usage_seconds_total{namespace="socialnetwork", container=~"nginx.*"}[2m])) / sum(kube_pod_container_resource_limits{resource="cpu", namespace="socialnetwork", container=~"nginx.*", service="kube-state-metrics"}) * 100',
+    "cpu_utilization_text": 'sum(rate(container_cpu_usage_seconds_total{namespace="socialnetwork", container=~"text-service.*"}[2m])) / sum(kube_pod_container_resource_limits{resource="cpu", namespace="socialnetwork", container=~"text-service.*", service="kube-state-metrics"}) * 100',
+    "cpu_utilization_user_mention": 'sum(rate(container_cpu_usage_seconds_total{namespace="socialnetwork", container=~"user-mention.*"}[2m])) / sum(kube_pod_container_resource_limits{resource="cpu", namespace="socialnetwork", container=~"user-mention.*", service="kube-state-metrics"}) * 100',
     
     # Replicas per Service
     "replicas_compose": 'count by (app) (kube_pod_info{pod=~"compose.*", app_kubernetes_io_instance="kube-state-metrics"})',
@@ -47,3 +47,6 @@ PROMETHEUS_QUERIES = {
     "replicas_text": 'count by (app) (kube_pod_info{pod=~"text-service.*", app_kubernetes_io_instance="kube-state-metrics"})',
     "replicas_user_mention": 'count by (app) (kube_pod_info{pod=~"user-mention.*", app_kubernetes_io_instance="kube-state-metrics"})'
 }
+
+# nginxplus_upstream_server_response_time
+# nginxplus_upstream_server_header_time
